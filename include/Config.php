b747cc246f8b92813442de8ace68f99f06bc62c6
paul.murdoch <paul@adloc8tor.com>
2016-11-24 11:44:44 +0200

    changes for time clock and String input to server

--- include/Config.php
+++ include/Config.php
@@ -2,10 +2,10 @@
 /**
  * Database configuration
  */
-define('DB_USERNAME', 'devbeanc_suser');
-define('DB_PASSWORD', '2ibo%4iHe!ri~');
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'devbeanc_salamandra');
+define('DB_USERNAME', '*****');
+define('DB_PASSWORD', '*****');
+define('DB_HOST', '******');
+define('DB_NAME', '*****');
 
 define('USER_CREATED_SUCCESSFULLY', 0);
 define('USER_CREATE_FAILED', 1);
